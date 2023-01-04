dd3782bb8c2a3bd433d573921c1c883bf761e92e
Hasan-Rifat <developerhr000.com>
2023-01-04 17:29:11 +0600

    setup done

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require("express");
-const app = express();
-const port = 3000;
+const cors = require("cors");
+const { connectToServer } = require("./utils/db.connect");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// payment
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// mongodb
+connectToServer((error) => {
+  if (!error) {
+    app.listen(port, () =>
+      console.log(`Example app listening on port ${port}`)
+    );
+  } else {
+    console.log(error);
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+// all routes
+app.get("/", (req, res) => {
+  res.send("hello world...");
+});
+
+app.all("*", (req, res) => {
+  res.send("not found");
 });
