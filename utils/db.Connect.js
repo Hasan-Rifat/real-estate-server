0158d834309656bdd727fa153ab0434091938b6b
Hasan-Rifat <developerhr000@gmail.com>
2023-01-10 01:51:28 +0600

    everything is complete

--- utils/db.Connect.js
+++ utils/db.Connect.js
@@ -2,10 +2,8 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const dbConnect = async () => {
-  mongoose.set("strictQuery", false);
-  mongoose.connect(process.env.ATLAS_URI, () => {
-    console.log("Connected to MongoDB");
-  });
+  mongoose.set("strictQuery", true);
+  mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true });
 };
 
 module.exports = dbConnect;
